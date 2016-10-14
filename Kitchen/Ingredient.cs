1d1cbfb00591ce13195b9140b1626278f9a8e41b
K_Kuba <195623@edu.p.lodz.pl>
2016-10-14 23:10:56 +0200

    Initial upload.

---
+++ Kitchen/Ingredient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen
+{
+    
+
+    class Ingredient
+    {
+        public Ingredient(string name, int ingredientID)
+        {
+            this.name = name;
+            this.ingredientID = ingredientID;
+        }
+
+        private int ingredientID;
+        private string name;
+    }
+}
