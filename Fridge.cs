1f2d9fdab1b0b78327d66ac89d04e3a92122f7e5
K_Kuba <195623@edu.p.lodz.pl>
2016-10-15 00:25:48 +0200

    Upload.

---
+++ Fridge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen
+{
+    class Fridge
+    {
+        public Fridge() { }
+
+        public void Request_Ingredients(List<string> orderNames)
+        {
+            // send list of names to outside database
+        }
+
+        public List<Ingredient> Return_Ingredients()
+        {
+            return this.ingredients;
+        }
+
+        public void Add_Ingredients(List<string> namedIngredients)
+        {
+            // convert names to Ingredients
+            for( int i = 0; i<namedIngredients.Count; i++ )
+            {
+                ingredients.Add(new Ingredient(namedIngredients[i],i+1));
+            }
+
+            //List<Ingredient> newIngredients = Names_To_Ingredients(namedIngredients);
+
+            //this.ingredients.AddRange(newIngredients);
+        }
+
+        public void Remove_Ingredients(List<string> namedIngredients)
+        {
+            // Remove ingredients based on name                                    
+        }
+
+        private List<Ingredient> ingredients;
+        private int previousID = 0;
+    }
+}
