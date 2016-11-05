8c0da00199c52e46f7b0c961c1c0cbbc9ae270d7
K_Kuba <195623@edu.p.lodz.pl>
2016-11-05 21:21:35 +0100

    Add Recipe

---
+++ Kitchen/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen
+{
+    class Recipe
+    {
+        public Recipe(string name, List<string> ingredientNames )
+        {
+            this.name = name;
+            this.ingredientNames = ingredientNames;
+        }
+
+        private string name;
+        private List<string> ingredientNames;
+    }
+}
